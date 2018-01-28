0da41f135e4f15da76b06b1d962d8753cdc0c6e3
Tim Cooper <tim.cooper@layeh.com>
2018-01-27 22:17:58 -0400

    replace manual instructions with automated script
    fixes #6

--- README.md
+++ README.md
@@ -1,97 +1,21 @@
 # Digital Ocean: Alpine Linux
 
-This guide instructs you on how to put [Alpine Linux](http://alpinelinux.org/) on a [Digital Ocean](https://m.do.co/c/a0f96edad652) (referral link) droplet.
+Script to install [Alpine Linux](http://alpinelinux.org/) on a [Digital Ocean](https://m.do.co/c/a0f96edad652) (referral link) droplet.
 
-## Requirements
+## Instructions
 
-- Digital Ocean account
-- Local Docker installation (used to generate the Alpine root filesystem)
+1. Create a new Debian 9.3 x64 droplet on Digital Ocean
+2. Download and run `digitalocean-alpine.sh` onto the droplet:
 
-## Process
-
-### Generate Alpine root file system
-
-1. Ensure Docker is running locally.
-2. Download and unzip [`gliderlabs/docker-alpine`](https://github.com/gliderlabs/docker-alpine).
-    - `wget -O docker-alpine-master.zip https://github.com/gliderlabs/docker-alpine/archive/master.zip`
-    - `unzip docker-alpine-master.zip`
-3. Build the builder.
-    - `docker build -t docker-alpine-builder docker-alpine-master/builder/`
-4. Build the root file system (change `v3.3` to the Alpine version you want to build).
-    - `docker run --name alpine-builder docker-alpine-builder -r v3.4`
-5. Copy the root file system from the container.
-    - `docker cp alpine-builder:/rootfs.tar.gz .`
-6. (Optional) Clean up builder.
-    - `docker rm alpine-builder`
-    - `docker rmi docker-alpine-builder`
-    - `rm -rf docker-alpine-master{,.zip}`
-
-You should now have `rootfs.tar.gz` in your current directory.
-
-### Prepare droplet
-
-1. Create droplet.
-    - In your Digital Ocean control panel, click "Create Droplet".
-    - Select the "Debian 7.11 x64" image, fill in the rest of your information, and click "Create Droplet".
-2. Transfer `rootfs.tar.gz` to the droplet.
-    - `scp rootfs.tar.gz root@<IP address>:`
-3. SSH into the droplet.
-    - `ssh root@<IP address>`
-4. Inside the droplet, extract the Alpine files onto your hard drive.
-    - `mkdir /alpine`
-    - `tar xf rootfs.tar.gz -C /alpine`
-    - `poweroff`
-
-### Setup root file system
-
-1. In your Digital Ocean droplet control panel, click "Kernel".
-2. Click "Mount Recovery Kernel".
-3. Switch on your droplet.
-4. Click "Console".
-5. In the recovery console, move the extracted Alpine files into the root of your drive.
-    - `mkdir /mnt`
-    - `mount -t ext4 /dev/vda1 /mnt`
-    - `mv /mnt/etc/network/interfaces /mnt/alpine/etc/network/`
-    - `mv /mnt/root/.ssh/ /mnt/alpine/root/`
-    - `mv /mnt/etc/hostname /mnt/alpine/etc/`
-    - `mv /mnt/etc/fstab /mnt/alpine/etc/`
-    - `mv /mnt/alpine/ /tmp/`
-    - `rm -rf /mnt/*`
-    - `mv /tmp/alpine/* /mnt/`
-    - `poweroff`
-6. Close the recovery console.
-7. Select a kernel (e.g. search for "3.2.0-4-amd64"), then click "Change".
-8. Switch on your droplet.
-
-### Configuring Alpine
-
-1. Click "Console" in your droplet control panel.
-2. Login as `root`.
-3. Enable writing to the file system.
-    - `mount -o rw,remount /dev/vda1 /`
-    - `vi /etc/fstab`
-    - Set the 4th column (options) of the `/` mount point to `defaults`
-    - Save and exit.
-4. Configure networking and SSH.
-    - `setup-dns`
-    - `service networking restart`
-    - `apk update`
-    - `setup-sshd`
-5. Enable services.
-    - `rc-update add hostname boot`
-    - `rc-update add networking boot`
-    - `rc-update add urandom boot`
-    - `rc-update add crond`
-    - `rc-update add swap boot`
-6. Reboot.
-    - `reboot`
-7. Close the console.
-
-After removing the old SSH fingerprint from your local machine's `~/.ssh/known_hosts` file, you should now be able to SSH into your droplet.
+```sh
+wget https://github.com/bontibon/digitalocean-alpine/raw/master/digitalocean-alpine.sh
+chmod +x digitalocean-alpine.sh
+./digitalocean-alpine.sh
+```
 
 ## License
 
-[Creative Commons Attribution 4.0 International License](https://creativecommons.org/licenses/by/4.0/)
+MPL 2.0
 
 ## Author
 
---
+++ digitalocean-alpine.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -e
+
+if [ "$1" = "--step-chroot" ]; then
+	echo -n "  Installing packages..." >&2
+	apk update >/dev/null 2>/dev/null
+	apk add alpine-base linux-virthardened syslinux grub grub-bios >/dev/null 2>/dev/null
+	echo " Done" >&2
+
+	echo -n "  Configuring network and services..." >&2
+	IP_ADDR=$(wget -q -O- http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+	IP_NETMASK=$(wget -q -O- http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/netmask)
+	IP_GATEWAY=$(wget -q -O- http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/gateway)
+
+	cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+    address $IP_ADDR
+    netmask $IP_NETMASK
+    gateway $IP_GATEWAY
+EOF
+
+	setup-sshd -c openssh >/dev/null 2>/dev/null || true
+
+	rc-update add hostname boot >/dev/null 2>/dev/null
+	rc-update add networking boot >/dev/null 2>/dev/null
+	rc-update add urandom boot >/dev/null 2>/dev/null
+	rc-update add crond default >/dev/null 2>/dev/null
+	rc-update add swap boot >/dev/null 2>/dev/null
+	echo " Done" >&2
+
+	echo -n "  Configuring bootloader..." >&2
+	sed -i -e 's/^root=.*$/root=\/dev\/vda1/' /etc/update-extlinux.conf
+
+	DRIVE_UUID=$(blkid -o value -s UUID /dev/vda1)
+
+	cat <<EOF >> /etc/grub.d/40_custom
+menuentry "Alpine Linux" {
+    set root=(hd0,1)
+    linux /boot/vmlinuz root=UUID=$DRIVE_UUID modules=ext4 quiet
+    initrd /boot/initramfs-vanilla
+}
+EOF
+
+	grub-install /dev/vda >/dev/null 2>/dev/null
+	grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>/dev/null
+
+	sync
+	echo " Done" >&2
+
+	rm -f "$0"
+
+	exit
+fi
+
+if [ "$1" != "--rebuild" ]; then
+	echo "usage: digitalocean-alpine --rebuild" >&2
+	echo "   Rebuild the current droplet with Alpine Linux" >&2
+	echo >&2
+	echo "   WARNING: This is a destructive operation. You will lose your data." >&2
+	echo "            This script has only been tested with Debian 9.3 x64 droplets" >&2
+	exit 1
+fi
+
+SCRIPTPATH="$(realpath "$0")"
+
+echo -n "Downloading Alpine 3.7.0..." >&2
+wget -q -O /tmp/rootfs.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.7/releases/x86_64/alpine-minirootfs-3.7.0-x86_64.tar.gz
+echo " Done" >&2
+
+echo -n "Creating mount points..." >&2
+mount -o rw,remount --make-rprivate /dev/vda1 /
+mkdir /tmp/tmpalpine
+mount none /tmp/tmpalpine -t tmpfs
+echo " Done" >&2
+
+echo -n "Extracting Alpine..." >&2
+tar xzf /tmp/rootfs.tar.gz -C /tmp/tmpalpine
+cp "$SCRIPTPATH" /tmp/tmpalpine/tmp/digitalocean-alpine.sh
+echo " Done" >&2
+
+echo -n "Copying existing droplet configuration..." >&2
+cp /etc/fstab /tmp/tmpalpine/etc
+cp /etc/hostname /tmp/tmpalpine/etc
+cp /etc/resolv.conf /tmp/tmpalpine/etc
+grep -v ^root: /tmp/tmpalpine/etc/shadow > /tmp/tmpalpine/etc/shadow.bak
+mv /tmp/tmpalpine/etc/shadow.bak /tmp/tmpalpine/etc/shadow
+grep ^root: /etc/shadow >> /tmp/tmpalpine/etc/shadow
+cp -r /etc/ssh /tmp/tmpalpine/etc
+cp -r /root/.ssh /tmp/tmpalpine/root
+echo " Done" >&2
+
+echo -n "Changing to new root..." >&2
+mkdir /tmp/tmpalpine/oldroot
+pivot_root /tmp/tmpalpine /tmp/tmpalpine/oldroot
+cd /
+echo " Done" >&2
+
+echo -n "Rebuilding file systems..." >&2
+mount --move /oldroot/dev /dev
+mount --move /oldroot/proc /proc
+mount --move /oldroot/sys /sys
+mount --move /oldroot/run /run
+
+rm -rf /oldroot/*
+
+cp -r /bin /etc /home /lib/ /media /mnt/ /root /sbin /srv /tmp /usr /var /oldroot
+
+mkdir /oldroot/dev /oldroot/proc /oldroot/sys /oldroot/run
+
+mount -t proc proc /oldroot/proc
+mount -t sysfs sys /oldroot/sys
+mount -o bind /dev /oldroot/dev
+
+echo " Done" >&2
+
+echo "chroot configuration..." >&2
+chroot /oldroot /bin/ash /tmp/digitalocean-alpine.sh --step-chroot
+
+echo "Rebooting system. You should be able to reconnect shortly."  >&2
+reboot
+sleep 1
+reboot
