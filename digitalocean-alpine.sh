18029c3a5aea8285ee408cdb30dedb1eec34159d
Tim Cooper <tim.cooper@layeh.com>
2018-01-28 09:45:03 -0400

    abort rebuild if Alpine could not be downloaded

--- digitalocean-alpine.sh
+++ digitalocean-alpine.sh
@@ -101,6 +101,10 @@ fi
 
 echo -n "Downloading Alpine 3.7.0..." >&2
 wget -q -O /tmp/rootfs.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.7/releases/x86_64/alpine-minirootfs-3.7.0-x86_64.tar.gz
+if [ "$?" -ne 0 ]; then
+	echo "Could not download Alpine. Exiting." >&2
+	exit 1
+fi
 echo " Done" >&2
 
 echo -n "Creating mount points..." >&2
