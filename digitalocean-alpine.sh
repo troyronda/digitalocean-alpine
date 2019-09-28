ed3513728d1cebe14f6289e58fb5fb5cc4c9341a
Tim Cooper <tim.cooper@layeh.com>
2019-09-28 16:41:51 -0500

    update to debian-10

--- digitalocean-alpine.sh
+++ digitalocean-alpine.sh
@@ -73,7 +73,7 @@ if [ "$1" != "--rebuild" ]; then
 	echo "   Rebuild the current droplet with Alpine Linux" >&2
 	echo >&2
 	echo "   WARNING: This is a destructive operation. You will lose your data." >&2
-	echo "            This script has only been tested with Debian 9.7 x64 droplets." >&2
+	echo "            This script has only been tested with debian-10 x64 droplets." >&2
 	exit 1
 fi
 
